cdde2d7c8059ecead04657aa3a5293f1d263a7de
Sourabh <s1905singh@gmail.com>
2022-03-08 12:54:36 +0530

    added comments in the code

--- src/games/Card.java
+++ src/games/Card.java
@@ -4,26 +4,25 @@ public class Card {
 	String name;
 	int number;
 	boolean is_face, is_spade;
-	
-    public Card(String name, int number){
-        this.name = name;
-        this.number = number;
-        this.is_face = false;
-        
-        if(number == 1 || number >= 11){   // Ace and Face Cards
-            this.is_face = true;
-        }
 
-        this.is_spade = false;
-        if(name == "SPADE"){
-            this.is_spade = true;
-        }
-    }
-    
+	public Card(String name, int number) { // Constructor
+		this.name = name;
+		this.number = number;
+		
+		this.is_face = false;
+		if (number == 1 || number >= 11) {  //ace and face Cards  //consider an ace card as face card
+			this.is_face = true;
+		}
+
+		this.is_spade = false;
+		if (name == "s") {
+			this.is_spade = true;
+		}
+	}
+
 	@Override
 	public String toString() {
-		return name+number;
+		return name + number;
 	}
-    
-	
+
 }
--- src/games/CardsGame.java
+++ src/games/CardsGame.java
@@ -5,19 +5,17 @@ import java.util.ArrayList;
 public class CardsGame {
 
 	public static void main(String[] args) {
-		ArrayList<Card> cards = new ArrayList<Card>();
-		
-		String[] name = {"SPADE", "HEART", "DIAMOND", "CLUB"};
-	    for(String n : name){
-	        for(int card_number = 1; card_number <= 13; card_number++){
-	            cards.add(new Card(n, card_number));
-	        }
-	    }
-	    
-	    Shuffle shuffle = new Shuffle();
-	    shuffle.run_infinite(cards);
+		ArrayList<Card> cards = new ArrayList<Card>(); // declaring an ArrayList of type Card
 
-		
+		String[] name = { "s", "h", "d", "c" };
+		for (String n : name) { // adding 52 playing cards in the ArrayList of cards
+			for (int card_number = 1; card_number <= 13; card_number++) {
+				cards.add(new Card(n, card_number));
+			}
+		}
+
+		Shuffle shuffle = new Shuffle(); // declaring an instance of Shuffle class
+		shuffle.run_infinite(cards);
 	}
 
 }
